60ebefd3b32f9c3b1a3a6f5d8702336af0fc150c
Idoia <idoia.reinares@epitech.eu>
2024-09-24 22:55:23 +0200

    Updated sorter

--- public/electron.js
+++ public/electron.js
@@ -34,9 +34,9 @@ function createWindow() {
   }
 
   // Automatically open Chrome's DevTools in development mode.
-  // if (!app.isPackaged) {
+  if (!app.isPackaged) {
     mainWindow.webContents.openDevTools();
-  // }
+  }
 
   mainWindow.on('closed', () => (mainWindow = null))
 }
