aae6c09027210f08ccd37ec0bfd3ac5c5a7cfd6f
Idoia <idoia.reinares@epitech.eu>
2024-09-25 20:43:27 +0200

    Updated files

--- public/electron.js
+++ public/electron.js
@@ -2,9 +2,8 @@ import {app, BrowserWindow} from 'electron'
 import path from 'path'
 import url from 'url'
 import { fileURLToPath } from 'url';
-import { createServer } from 'http-server'; // Importer http-server
+import { createServer } from 'http-server';
 
-// Créer l'équivalent de __dirname dans un environnement ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -16,24 +15,21 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,  // Permet de désactiver l'isolement du contexte
-      enableRemoteModule: true,  // Permet d'utiliser des modules comme 'path'
+      contextIsolation: false,
+      enableRemoteModule: true,
     },
+    icon: __dirname + '/components/MainLogo/logo.png',
   })
 
   if (app.isPackaged) {
-    // Démarrer un serveur local pour servir le dossier build
     const server = createServer({ root: path.join(__dirname, '../build') });
     server.listen(3001, '127.0.0.1', () => {
-      // Charger l'application via le serveur HTTP local
       mainWindow.loadURL('http://127.0.0.1:3001');
     });
   } else {
-    // En développement, chargez l'application via localhost
     mainWindow.loadURL('http://localhost:3000');
   }
 
-  // Automatically open Chrome's DevTools in development mode.
   if (!app.isPackaged) {
     mainWindow.webContents.openDevTools();
   }
