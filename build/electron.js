2993c45882dd7d7e445d5b41f0fc3d6b17e2af1c
Idoia <idoia.reinares@epitech.eu>
2024-10-05 20:07:23 +0200

    Updated files

---
+++ build/electron.js
@@ -0,0 +1,50 @@
+import {app, BrowserWindow} from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { createServer } from 'http-server';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  })
+
+  if (app.isPackaged) {
+    const server = createServer({ root: path.join(__dirname, '../build') });
+    server.listen(3001, '127.0.0.1', () => {
+      mainWindow.loadURL('http://127.0.0.1:3001');
+    });
+  } else {
+    mainWindow.loadURL('http://localhost:3000');
+  }
+
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
