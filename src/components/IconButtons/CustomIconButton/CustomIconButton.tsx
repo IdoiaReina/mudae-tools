af0907bdffcf37baba7cf5618663ef88c682d937
Idoia <idoia.reinares@epitech.eu>
2024-10-06 15:12:37 +0200

    Updated custom icon button

--- src/components/IconButtons/CustomIconButton/CustomIconButton.tsx
+++ src/components/IconButtons/CustomIconButton/CustomIconButton.tsx
@@ -1,23 +1,54 @@
 /* Framework imports -------------------------------------------------------- */
 import React from 'react'
+import styled from '@emotion/styled'
 
 /* Component imports -------------------------------------------------------- */
-import CustomIconButtonContainer from './CustomIconButtonContainer'
+import {
+  Button,
+  Tooltip,
+  type ButtonProps,
+} from '@mui/material'
 
 /* Type imports ------------------------------------------------------------- */
-import type { ButtonProps } from '@mui/material'
 import type { SvgIconComponent } from '@mui/icons-material'
+
+/* Styled components -------------------------------------------------------- */
+const CustomIconButtonContainer = styled(Button)`
+  min-width: 38px;
+  width: 38px;
+  min-height: 38px;
+  height: 38px;
+  margin: 0 2px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    font-size: 26px;
+  }
+`
 
 /* Component declaration ---------------------------------------------------- */
 interface CustomIconButtonProps extends ButtonProps {
   Icon: SvgIconComponent;
+  label?: string;
 }
 
-const CustomIconButton: React.FC<CustomIconButtonProps> = ({ Icon, ...rest }) => {
+const CustomIconButton: React.FC<CustomIconButtonProps> = ({ Icon, label, ...rest }) => {
   return (
-    <CustomIconButtonContainer {...rest}>
-      <Icon />
-    </CustomIconButtonContainer>
+    <Tooltip
+      arrow
+      placement="top"
+      title={label}
+    >
+      <CustomIconButtonContainer
+        {...rest}
+        ref={rest.ref}
+      >
+        <Icon />
+      </CustomIconButtonContainer>
+    </Tooltip>
   )
 }
 
