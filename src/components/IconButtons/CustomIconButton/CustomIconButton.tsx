99f0e4f582b48cc62b73675977e6b1d6d7f2691e
Idoia <idoia.reinares@epitech.eu>
2024-10-06 15:13:43 +0200

    Updated custom icon button

--- src/components/IconButtons/CustomIconButton/CustomIconButton.tsx
+++ src/components/IconButtons/CustomIconButton/CustomIconButton.tsx
@@ -1,33 +1,15 @@
 /* Framework imports -------------------------------------------------------- */
 import React from 'react'
-import styled from '@emotion/styled'
 
 /* Component imports -------------------------------------------------------- */
 import {
-  Button,
   Tooltip,
   type ButtonProps,
 } from '@mui/material'
+import CustomIconButtonContainer from './CustomIconButtonContainer'
 
 /* Type imports ------------------------------------------------------------- */
 import type { SvgIconComponent } from '@mui/icons-material'
-
-/* Styled components -------------------------------------------------------- */
-const CustomIconButtonContainer = styled(Button)`
-  min-width: 38px;
-  width: 38px;
-  min-height: 38px;
-  height: 38px;
-  margin: 0 2px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  svg {
-    font-size: 26px;
-  }
-`
 
 /* Component declaration ---------------------------------------------------- */
 interface CustomIconButtonProps extends ButtonProps {
